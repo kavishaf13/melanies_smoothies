d0ab75d809d6fac82e5ba4b072076adeaa467036
kavishaf13 <kavishafernando@kubrickgroup.com>
2024-07-12 17:49:32 +0100

    Update streamlit_app.py
    adding the nutritional info

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,7 @@ if time_to_insert:
         
     #st.write(my_insert_stmt)
     #st.stop()
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
